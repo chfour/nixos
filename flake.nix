803feb1bd2f60f1ea551f81c66073e914a831b3e
chfour <chfourchfour@protonmail.com>
2023-10-30 15:28:22 +0100

    packages brokey and the fix isnt in nixos-unstable yet

--- flake.nix
+++ flake.nix
@@ -1,15 +1,18 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    nixpkgs-master.url = "github:NixOS/nixpkgs/master";
     
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
   
-  outputs = { self, nixpkgs, nixos-hardware, home-manager, ... }: {
+  outputs = { self, nixpkgs, nixpkgs-master, nixos-hardware, home-manager, ... }: {
     nixosModules = {
       declarativeHome = { ... }: {
         # big thank you to https://determinate.systems/posts/declarative-gnome-configuration-with-nixos !!!
@@ -22,8 +25,11 @@
       # theres probably a better way to do this lol
       overlays = ./overlays;
     };
-    nixosConfigurations."foxbox" = nixpkgs.lib.nixosSystem {
+    nixosConfigurations."foxbox" = nixpkgs.lib.nixosSystem rec {
       system = "x86_64-linux";
+      specialArgs = {
+        pkgs-master = nixpkgs-master.legacyPackages.${system};
+      };
       modules = with self.nixosModules; [
         overlays
         ./machines/foxbox
