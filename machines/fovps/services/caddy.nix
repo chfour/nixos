d2333d839029b4e4e70d564e2cc391ba1d992e3b
chfour <chfourchfour@protonmail.com>
2023-11-10 12:07:54 +0100

    fopvps/caddy: add my website

--- machines/fovps/services/caddy.nix
+++ machines/fovps/services/caddy.nix
@@ -1,13 +1,23 @@
-{ ... }:
-
+{ pkgs, ... }:
+let
+  websiteSource = pkgs.fetchFromGitHub {
+    owner = "chfour";
+    repo = "website-static";
+    rev = "c27c5fc110cae32a143d021e54506a71843007af";
+    hash = "sha256-1k+KOZDEi3J81HUYx0kY9V2QyRyZylk5kYwW3extM9I=";
+  };
+in
 {
   services.caddy.enable = true;
   services.caddy.virtualHosts = {
-    "eeep.ee" = {
-      extraConfig = ''
-        respond "test hello world!\n" 200
-      '';
-    };
+    "eeep.ee".extraConfig = ''
+      root * ${websiteSource}
+      encode zstd gzip
+      file_server
+    '';
+    
+
+
   };
   networking.firewall.allowedTCPPorts = [ 80 443 ];
 }
