1a7058d3f3c893db373527635aca49b05afd7dfd
chfour <chfourchfour@protonmail.com>
2024-04-14 16:52:37 +0200

    ip adresses of the new vps which i again forgot to commit

--- machines/fovps/networking.nix
+++ machines/fovps/networking.nix
@@ -6,18 +6,21 @@
   systemd.network.networks."10-wan" = {
     matchConfig.Name = "ens18";
 
+    networkConfig.DHCP = "ipv4";
+    
     # apparently ipv6s are done manually
     # https://contabo.com/blog/adding-ipv6-connectivity-to-your-server/
     # idk anymore skull emoji
     
-    networkConfig = {
-      DHCP = "ipv4";
-    };
     address = [
-      "2a02:c207:2087:819::1/64"
+      "2a02:c207:2177:8888::1/64"
     ];
     routes = [
       { routeConfig.Gateway = "fe80::1"; }
+    ];
+    dns = [
+      "2a02:c207::1:53"
+      "2a02:c207::2:53"
     ];
   };
 }
