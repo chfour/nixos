7b4fd3af2141ceb290fd5408b457ccff93398a28
chfour <chfourchfour@protonmail.com>
2024-11-13 12:22:26 +0100

    remove services.matrix-sliding-sync (not sure if it was ever needed)

--- machines/fovps/services/synapse.nix
+++ machines/fovps/services/synapse.nix
@@ -4,7 +4,6 @@ with builtins;
 let
   domain = "eeep.ee";
   synapsePort = 8008; # todo: unix socket maybe?
-  slidingPort = 8009;
 
   synapse = config.services.matrix-synapse;
   synapseUnit = config.systemd.services.matrix-synapse.serviceConfig;
@@ -22,13 +21,13 @@ in
     # automatically created
     registration_shared_secret_path = "${synapse.dataDir}/.env.synapse-reg";
   };
-  
+
   #services.coturn = {
   #  enable = true;
   #  use-auth-secret = true;
   #  static-auth-secret-file = "";
   #};
-  
+
   services.matrix-synapse.settings.database.name = "psycopg2";
   services.postgresql = let args = synapse.settings.database.args; in {
     enable = true;
@@ -37,44 +36,12 @@ in
     ensureUsers = [ { name = args.user; ensureDBOwnership = true; } ];
   };
 
-  services.matrix-sliding-sync = {
-    enable = true;
-    createDatabase = true;
-    settings = {
-      SYNCV3_SERVER = "https://localhost:${toString synapsePort}";
-      SYNCV3_BINDADDR = "127.0.0.1:${toString slidingPort}";
-    };
-    # https://stackoverflow.com/questions/42835750/systemd-script-environment-file-updated-by-execstartpre
-    environmentFile = "${synapseUnit.WorkingDirectory}/.env.sliding-sync";
-  };
-  systemd.services.matrix-sliding-sync = rec {
-    preStart = let
-      envFile = config.services.matrix-sliding-sync.environmentFile;
-    in ''
-      if ! [ -f "${envFile}" ]; then
-        echo -n 'SYNCV3_SECRET=' > ${envFile}
-        ${pkgs.openssl}/bin/openssl rand -hex 64 >> ${envFile}
-      fi
-    '';
-    serviceConfig = {
-      DynamicUser = lib.mkForce false;
-      User = synapseUnit.User;
-      Group = synapseUnit.Group;
-      WorkingDirectory = lib.mkForce synapseUnit.WorkingDirectory;
-      StateDirectory = lib.mkForce "";
-      EnvironmentFile = lib.mkForce "-${config.services.matrix-sliding-sync.environmentFile}";
-    };
-  };
-  
   services.matrix-synapse.settings.listeners = [
     {
       bind_addresses = [ "127.0.0.1" ];
       port = synapsePort;
       resources = [
-        {
-          compress = false;
-          names = [ "client" "federation" ];
-        }
+        { compress = false; names = [ "client" "federation" ]; }
       ];
       tls = false;
       type = "http";
@@ -85,8 +52,6 @@ in
   services.caddy.virtualHosts."${domain}".extraConfig = ''
     reverse_proxy /_matrix/* localhost:${toString synapsePort}
     reverse_proxy /_synapse/client/* localhost:${toString synapsePort}
-    
-    reverse_proxy /_matrix/client/unstable/org.matrix.msc3575/sync localhost:${toString slidingPort}
   '';
   services.caddy.virtualHosts."https://${domain}:8448".extraConfig = ''
     reverse_proxy /_matrix/* localhost:${toString synapsePort}
