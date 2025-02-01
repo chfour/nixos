44487cd5e5819ac2c26f682acba5d905fa0b985a
chfour <chfourchfour@protonmail.com>
2025-02-01 16:29:39 +0100

    make ai scraper bots fuck off

--- machines/fovps/services/caddy/default.nix
+++ machines/fovps/services/caddy/default.nix
@@ -8,25 +8,29 @@ in {
   services.caddy.extraConfig = ''
     (errors) {
       handle_errors {
-      	@custom_err file /{err.status_code}.html /err.html
-      	handle @custom_err {
-      		rewrite * {file_match.relative}
-      		file_server
-      	}
-      	respond "{err.status_code} {err.status_text}
+        @custom_err file /{err.status_code}.html /err.html
+        handle @custom_err {
+      	  rewrite * {file_match.relative}
+      	  file_server
+        }
+        respond "{err.status_code} {err.status_text}
 " # caddy why
+      }
+    }
+    (bots) {
+      # https://donotsta.re/objects/4ff43b3e-2a56-48f1-870b-48353cd90801
+      # > DON’T LET YOUR DREAMS STAY DREAMS!
+      @bots header_regexp User-Agent "GPTBot|ChatGPT-User|Google-Extended|CCBot|PerplexityBot|anthropic-ai|Claude-Web|ClaudeBot|Amazonbot|FacebookBot|Applebot-Extended|semrush|barkrowler|PetalBot|meta-externalagent|meta-externalfetcher|facebookexternalhit|facebookcatalog|Firefox/72\.0|babbar\.tech|BLEXBot|DotBot|ahrefs|DataForSeoBot|ImagesiftBot|Nexus 5X Build/MMB29P|Arc/|MJ12Bot|dataforseo-bot|MJ12bot"
+      handle @bots {
+        header X-Fuck-Off "PLEASE do!"
+        redir https://nbg1-speed.hetzner.com/10GB.bin?BALLS permanent
       }
     }
   '';
   services.caddy.virtualHosts = {
     "eeep.ee".extraConfig = ''
       import errors
-
-      # lol
-      redir /nixos /nixos/ permanent
-      handle_path /nixos/* {
-        redir * https://github.com/chfour/nixos/tree/main{uri}
-      }
+      import bots
 
       root * ${websiteDest}
       encode zstd gzip
@@ -35,6 +39,7 @@ in {
 
     "files.eeep.ee".extraConfig = ''
       import errors
+      import bots
 
       root * /srv/pub
       encode zstd gzip
