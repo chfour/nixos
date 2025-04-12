bf303d50d77d5b2a9c9f7e76c8ff2e0ab1650e60
chfour <chfourchfour@protonmail.com>
2025-04-13 00:42:55 +0200

    add ssh key for my phone

--- users/chfour/default.nix
+++ users/chfour/default.nix
@@ -15,7 +15,10 @@
     uid = 1000;
     extraGroups = [ "wheel" "plugdev" "dialout" ] ++ (lib.optional config.networking.networkmanager.enable "networkmanager");
 
-    openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICOvi+wFazV5fN/piUQ0OM84W71TOuedHcAOsT/oRP55'' ];
+    openssh.authorizedKeys.keys = [
+      ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICOvi+wFazV5fN/piUQ0OM84W71TOuedHcAOsT/oRP55 chfour+foxbox@eeep.ee''
+      ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDG7JztPvxcx1YzRKOKGCXx0u/TQO/QsPZL89VcP+UuN chfour+phone@eeep.ee''
+    ];
   };
 
   programs.zsh.enable = true; # todo: get rid of this if possible
