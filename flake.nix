c4f5c1b476cec9890681bbb7618ec18c064216eb
chfour <chfourchfour@protonmail.com>
2025-05-22 18:15:56 +0200

    update lix too

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
 
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0-3.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
